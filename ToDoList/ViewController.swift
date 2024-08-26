e01fdeb50bb825ad250261497ecaedf1368c1ba8
Artem <artyom.solovev@icloud.com>
2024-08-26 12:20:30 +0300

    remove Main.storyboard

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  ToDoList
-//
-//  Created by Artem on 26.08.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
